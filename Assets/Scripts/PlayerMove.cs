7f4431c53bcd02a0b76c51fcd8705929cfdb361e
GRANT CAMPBELL <2502471@uad.ac.uk>
2025-10-24 10:27:48 +0100

    Player Movement Script now allows movement
    Player can now move left and right

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2 (moveHorizontal, 0);
+        rb.AddForce(movement * speed);
     }
+
+    
 }
