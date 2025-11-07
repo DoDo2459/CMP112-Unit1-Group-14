1bbbac7bb12a95bbba6bb63aca226e95cdc8d1af
GRANT CAMPBELL <2502471@uad.ac.uk>
2025-11-07 10:11:46 +0000

    Hopefully score UI is fixed

--- Assets/Scripts/coinDestroy.cs
+++ Assets/Scripts/coinDestroy.cs
@@ -5,16 +5,14 @@ public class coinDestroy : MonoBehaviour
 {
     private static int score;
 
-    //Audio variables
-    private AudioSource source;
-    public AudioClip CoinSound;
+    
 
     public GameManager manager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        source = GetComponent<AudioSource>();
+        
         manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
     }
 
@@ -32,7 +30,7 @@ public class coinDestroy : MonoBehaviour
         if (other.CompareTag("Player"))
         {
 
-            source.PlayOneShot(CoinSound, 1.0f);
+            
             Destroy(gameObject);
             manager.addscore(1);
 
